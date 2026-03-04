bc5549f57518432542bd997c2da6e23fdfd70f5c
Denys Baliuk <denuk14228@gmail.com>
2026-03-05 00:11:38 +0200

    fifth try done project elves and dwarves

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-from app.main import calculate_team_total_rating, elves_concert, feast_of_the_dwarves
-from app.players.player import Player
-from app.players.elves.elf import Elf
-from app.players.elves.elf_ranger import ElfRanger
-from app.players.elves.druid import Druid
-from app.players.dwarves.dwarf import Dwarf
-from app.players.dwarves.dwarf_warrior import DwarfWarrior
-from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
+from app.main import (
+    calculate_team_total_rating,
+    elves_concert,
+    feast_of_the_dwarves
+)  # noqa: F401
+from app.players.player import Player  # noqa: F401
+from app.players.elves.elf import Elf  # noqa: F401
+from app.players.elves.elf_ranger import ElfRanger  # noqa: F401
+from app.players.elves.druid import Druid  # noqa: F401
+from app.players.dwarves.dwarf import Dwarf  # noqa: F401
+from app.players.dwarves.dwarf_warrior import DwarfWarrior  # noqa: F401
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith  # noqa: F401
